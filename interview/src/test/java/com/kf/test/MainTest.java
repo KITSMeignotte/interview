2dbea607f76fc533d0d7fd4aa2cc23cc9b49848f
meignotte_n <nicolas.meignotte@kingfisher.com>
2015-09-24 14:15:01 +0200

    Test

---
+++ interview/src/test/java/com/kf/test/MainTest.java
@@ -0,0 +1,21 @@
+package com.kf.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
+	public void testAssertTrue() {
+		Assert.assertTrue(true);
+	}
+
+}
